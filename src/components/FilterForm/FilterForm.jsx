e5d91f0817d952000f3406f3c9a58fee2257ade6
Jofi <jofi1@seznam.cz>
2024-03-30 20:41:46 +0100

    vytvoreni option z brands

--- src/components/FilterForm/FilterForm.jsx
+++ src/components/FilterForm/FilterForm.jsx
@@ -4,14 +4,18 @@ function FilterForm({ data }) {
   const [brands, setBrands] = useState([]); //nastaveni prazdneho pole s useEffect, aby se pole aktualizovalo vzdy kdyz se zmeni data
 
   useEffect(() => {
-    Array.from(new Set(data.map((boat) => boat.brand))); // nejrychlejsi zpusob jak vytvorit unikatnost: ze znacek je vytvoreno pole (data.map...), pro unikatnost (aby se neopakovaly) znacek je z nich vytvorena kolekce (Set), z kolekce je pak vytvoreno znovu pole (Array.from)
+    console.log(Array.from(new Set(data.map((boat) => boat.brand)))); // nejrychlejsi zpusob jak vytvorit unikatnost: ze znacek je vytvoreno pole (data.map...), pro unikatnost (aby se neopakovaly) znacek je z nich vytvorena kolekce (Set), z kolekce je pak vytvoreno znovu pole (Array.from)
   }, [data]);
 
   return (
     <fieldset>
       <legend>Boat filter</legend>
       <div>
-        <select></select>
+        <select>
+          {brands.map((brand) => (
+            <option key={brand}>{brand}</option>
+          ))}
+        </select>
       </div>
     </fieldset>
   );
