e7e1b5bfd61df7619f202f9c7a9e31cd4e91173d
Jofi <jofi1@seznam.cz>
2024-03-30 20:16:48 +0100

    nastaveni vyhledavani podle znacky

--- src/components/FilterForm/FilterForm.jsx
+++ src/components/FilterForm/FilterForm.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function FilterForm({ data }) {
+  const [brands, setBrands] = useState([]); //nastaveni prazdneho pole s useEffect, aby se pole aktualizovalo vzdy kdyz se zmeni data
+
+  useEffect(() => {
+    data.map((boat) => boat.brand);
+  }, [data]);
+
   return (
     <fieldset>
       <legend>Boat filter</legend>
+      <div>
+        <select></select>
+      </div>
     </fieldset>
   );
 }
