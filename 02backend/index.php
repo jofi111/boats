7b9aa9e0b6b41a1bcbff90ee5e44d5648c46002e
Jofi <jofi1@seznam.cz>
2024-04-28 20:24:46 +0200

    nastaveni headeru, metody connect a pozadavku na db a metody execute

--- 02backend/index.php
+++ 02backend/index.php
@@ -1,1 +1,14 @@
 <?php
+header("Acces-Control-Allow-Origin: *"); //jde o testovaci aplikaci v ramci meho webhostingu tak * povoluje vse, puvod muze byt odkudkoliv
+header("Acces-Control-Allow-Headers: *"); //povoluje vsechny hlavicky
+header("Acces-Control-Allow-Method: GET, POST, PUT, DELETE"); //povoluje cteni, zapis, zmenu, mazani
+header("Content-Type: application/json; charset=utf-8"); //komunikace mezi klientem a serverem pouze pomoci jsonu
+
+include("./DbConnect.php");
+
+$connection = new DbConnect();
+$database = $connection->connect(); //zavolani metody connect, kt vrati pdo objekt=pripojeni
+
+$sql = "SELECT * FROM boats";
+$stmt = $database->prepare($sql); //pozadavek=statement, do db se posle dotaz (viz o radek vyse)
+$stmt->execute(); //zavolani metody execute
